0c7dcf471d6e57add1ae246fb6408c95be05a657
Raphaël GONCALVES <contact@raphael-goncalves.fr>
2015-12-28 23:16:49 +0100

    add drush alias
    Drush alias

--- bootlocal.sh
+++ bootlocal.sh
@@ -7,5 +7,6 @@ mount -t vboxsf www /var/www
 echo 'alias composer='\''docker run -ti --rm -v $(pwd):/app composer/composer'\''' >> /home/docker/.ashrc
 echo 'alias php='\''docker exec -ti docker_web_1 php'\''' >> /home/docker/.ashrc
 echo 'alias phpunit='\''docker run -v $(pwd):/app phpunit/phpunit'\''' >> /home/docker/.ashrc
+echo 'alias drush='\''docker run -v $(pwd):/app drush/drush'\''' >> /home/docker/.ashrc
 
 echo 'cd /var/www' >> /home/docker/.ashrc
