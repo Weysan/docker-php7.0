79fde3de8290a773059383aeb40fdebc0d6314d0
Weysan <raphaelgoncalves@iMac-de-Raphael.local>
2016-05-14 15:55:36 +0200

    SSL support
    Adding SSL support. Self-signed certificate for dev ONLY
    Tutoriel : http://www.akadia.com/services/ssh_test_certificate.html

--- bootlocal.sh
+++ bootlocal.sh
@@ -2,7 +2,10 @@ cp /var/lib/boot2docker/docker-compose /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 
 mkdir /var/www
-mount -t vboxsf www /var/www
+mount -t vboxsf -o uid=33,gid=33 www /var/www
+
+echo 'cd /var/www' >> /home/docker/.ashrc
 
 echo 'alias composer='\''docker run -ti --rm -v $(pwd):/app composer/composer'\''' >> /home/docker/.ashrc
-echo 'cd /var/www' >> /home/docker/.ashrc
+echo 'alias php='\''docker exec -ti docker_web_1 php'\''' >> /home/docker/.ashrc
+echo 'alias phpunit='\''docker run -v $(pwd):/app phpunit/phpunit'\''' >> /home/docker/.ashrc
